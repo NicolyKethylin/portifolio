cc92637a003bc80221a0312620798be930d46d14
NicolyKethylin <kethylinswan@outlook.com>
2024-12-17 14:36:15 -0300

    texto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from './componentes/Layout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+// Import pages
+import { Home } from "./pages/Home"
+import Footer from './componentes/Footer';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ return (
+  <>
+   <BrowserRouter>
+
+    <Layout />
+    <div className="container">
+     <Routes>
+      <Route path="/" element={<Home />} />
+     </Routes>
     </div>
-  );
+    <Footer />
+   </BrowserRouter>
+  </>
+ );
 }
 
 export default App;
---
+++ src/componentes/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Buttons = (props) => {
+ return (
+  <div>
+   <button>{props.text}</button>
+  </div>
+ )
+}
+export default Buttons
